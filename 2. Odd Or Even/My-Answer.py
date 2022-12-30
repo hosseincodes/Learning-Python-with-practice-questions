3f8a71690dc6bb7925069f29f45de344012c4cf5
Hossein Akbari <hosseinakbari506@gmail.com>
2022-12-30 23:38:48 +0330

    Add multiples of 4

--- 2. Odd Or Even/My-Answer.py
+++ 2. Odd Or Even/My-Answer.py
@@ -1,8 +1,8 @@
-number = input("enter a number: ")
+number = int(input("enter a number: "))
 
-OddOrEven = int(number) % 2
-
-if OddOrEven == 0:
+if number % 4 == 0:
+    print("This number is a multiple of 4. ")
+elif number % 2 == 0:
     print("This number is Even. ")
 else:
     print("This number is Odd. ")
